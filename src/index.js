46a5cfb12ec06e9c9b9c9048fe7ae929ff14c51b
EsauKip <esau.kiprono@student.moringaschool.com>
2022-09-15 15:03:54 +0300

    create navigation bar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App'
-const root = ReactDom.createRoot(document.getElementByID('root'));
+
+const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
     <App />
     </BrowserRouter>
     
-)
+);
