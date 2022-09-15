c4639a0a60e0a3a7929374871b051f88f2f231a8
EsauKip <esau.kiprono@student.moringaschool.com>
2022-09-15 11:51:02 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material'
 
+import './App.css';
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px">
+    NavBar
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
+    </Routes>
+    </Box>
   )
 }
 
-export default App
+export default App 
