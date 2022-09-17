22bf32226243d3aff66c6b4895de77bb02e17d87
EsauKip <esau.kiprono@student.moringaschool.com>
2022-09-17 11:41:43 +0300

    add typography,textfield and button

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Button, Typography, TextField } from "@mui/material";
 
 const SearchExercises = () => {
   return (
-    <div>
-      searchExercises
-    </div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Amazing Exercises You did'nt
+        <br /> know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          className="textField"
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "1170px", xs: "350px" },
+            borderRadius: "4opx",
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search prefered Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#fff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
