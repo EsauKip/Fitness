31d3167c53de70cbbc64b57a027bf398e410721a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-09-22 23:35:01 +0300

    add navigation arrows

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { Stack, Typography } from '@mui/material';
-import Icon from '../assets/icons/gym.png';
-const BodyPart = (item,setBodyPart,bodyPart) => {
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-   <Stack
-   type="button"
-   alignItems="center"
-   justifyContent="center"
-   className="bodyPart-card"
-   sx={
-    bodyPart === item ? {
-       borderTop: '4px solid #FF2625',
-       backgroundColor: '#281a3b'
-
-    }:''
-   }
-   >
-   <img src={Icon} alt="dumbell" styles={{width:"40px",height:"40px"}}/>
-   </Stack>
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #FF2625" : "",
+        backgroundColor: "#281a3b",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
+    >
+      <img
+        src={Icon}
+        alt="dumbell"
+        style={{ width: "40px", height: "40px" }}
+      />
+      <Typography fontSize="24px" fontWeight="bold" textTransform="capitalize">{item}
+      </Typography>
+    </Stack>
   );
-}
+};
 
 export default BodyPart;
