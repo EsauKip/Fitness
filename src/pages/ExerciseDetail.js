03584cdee0df69f6df404b15ae238880ac0f0e07
EsauKip <esau.kiprono@student.moringaschool.com>
2022-09-26 14:38:43 +0300

    display the data

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -13,7 +13,7 @@ const ExerciseDetail = () => {
 const fetchExercisesData = async () =>{
   const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
   const youtubeSearchUrl ='https://youtube-search-and-download.p.rapidapi.com';
-  const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`,exerciseOptions);
+  const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions);
   setExerciseDetail(exerciseDetailData)
 }
 fetchExercisesData();
