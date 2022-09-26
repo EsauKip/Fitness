a0af697632a51742bf315ae02f2d3b4a7c839be9
EsauKip <esau.kiprono@student.moringaschool.com>
2022-09-26 11:06:05 +0300

    add scroll effect to the pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,12 @@ import { exerciseOptions, fetchData } from '../utils/fetchData'
 import ExerciseCard from './ExerciseCard'
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   console.log(exercises);
+  const [currentPage, setCurrentPage]=useState(1);
+  const exercisesPerPage = 9;
+  const paginate =(e, value) =>{
+  setCurrentPage(value);
+  window.scrollTo({top:1800, behavior:'smooth'})
+  }
   return (
     <Box id="exercises"
       sx={{ mt: { lg: '110px' } }}
@@ -26,8 +32,9 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
             color="standard"
             shape="rounded"
             defaultPage={1}
-            count={Math.ceil(exercises.length / 9)}
-            
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
             size="large"
           />
         )}
